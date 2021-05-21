5333d3f299c6e08017bd82c158cd63df4d33a8c7
Nermin <nimamovic9@gmail.com>
2021-05-21 15:05:12 +0200

    try

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,14 +1,23 @@
 #! /usr/bin/env node
 
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
+const fs = require('fs')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+let body;
+
+try {
+    body = fs.readFileSync('./body.txt', 'utf8')
+    console.log(body)
+} catch (err) {
+    console.error(err)
+}
 
 const msg = {
     to: 'nimamovic9@gmail.com',
     from: 'nimamovic9@gmail.com',
     subject: 'Hello world',
-    text: 'Hello plain world!',
-    html: '<p>Hello HTML world!</p>',
+    text: body,
 };
 
 sgMail
