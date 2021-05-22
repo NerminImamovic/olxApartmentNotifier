5bdf95823fc3ef5276f2d14c7d4435423df03ce1
Nermin <nimamovic9@gmail.com>
2021-05-22 18:28:52 +0200

    sengrid mail sending

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -4,27 +4,26 @@ const sgMail = require('@sendgrid/mail');
 const fs = require('fs')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-let body;
-
 try {
-    body = fs.readFileSync('./body.txt', 'utf8')
-
+    let body = fs.readFileSync('./body.txt', 'utf8');
     body = body.replace(',', ' ,');
+    body = body.split(':', ':<br />');
+    body = body.split('KM').join('KM<br />');
 
-    console.log(body)
+    console.log(body);
+
+    const msg = {
+        to: 'nimamovic9@gmail.com',
+        from: 'nimamovic9@gmail.com',
+        subject: 'OLX Update - Stanovi',
+        text: body,
+        html: body,
+    };
+
+    sgMail
+        .send(msg)
+        .then(() => console.log('Mail sent successfully'))
+        .catch(error => console.error(error.toString()));
 } catch (err) {
-    console.error(err)
+    console.error(err);
 }
-
-const msg = {
-    to: 'nimamovic9@gmail.com',
-    from: 'nimamovic9@gmail.com',
-    subject: 'Hello world',
-    text: body,
-    html: body,
-};
-
-sgMail
-    .send(msg)
-    .then(() => console.log('Mail sent successfully'))
-    .catch(error => console.error(error.toString()));
